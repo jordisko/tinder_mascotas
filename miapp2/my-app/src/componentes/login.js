8e31b5eb5b6b5adf1ace90a5af7683037fd6362f
gabisita <gabitelega1997@gmail.com>
2022-09-20 16:21:26 +0200

    frontfinalgabi

--- miapp2/my-app/src/componentes/login.js
+++ miapp2/my-app/src/componentes/login.js
@@ -5,14 +5,17 @@ import Modal from 'react-bootstrap/Modal';*/
 import {useAuth0} from '@auth0/auth0-react';
 
 
-
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
+
 
   return <button className="botonlogin" onClick={() => loginWithRedirect()}>Inicia sesión</button>;
 };
 
 export default LoginButton;
+
+
+
 
 
 /*function Login() {
